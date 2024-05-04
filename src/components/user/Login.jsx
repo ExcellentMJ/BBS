b73d0af75a559d0a1b5547d41bd1490a946920ed
A-peaches <geunmi0521@gmail.com>
2024-05-04 11:28:14 +0900

    week9

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Row, Col, Form, InputGroup, Card, Button } from 'react-bootstrap'
+import {app} from '../../firebaseInit'
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
-  return (
-    <div>
-        <h1 className="my-5">로그인</h1>
-    </div>
-  )
+    const navi = useNavigate();
+    const [loading, setLoading] = useState(false);
+    const auth = getAuth(app);
+    const [form, setForm] = useState({
+            email : 'blue@test.com',
+            pass : '12341234'
+    });
+    const { email, pass } = form;
+    const onChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]:e.target.value
+        })
+    }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if(email === "" || pass === "") {
+            alert("이메일과 비밀번호를 입력하세요!");
+        } else {
+            //로그인 체크
+            setLoading(true);
+            signInWithEmailAndPassword(auth, email, pass)
+            .then(suceess=>{
+                alert('로그인 성공!');
+                setLoading(false);
+                sessionStorage.setItem('email', email);
+                navi("/");
+            })
+            .catch(error=>{
+                alert('에러 : ' + error.message);
+                setLoading(false);
+            })
+            
+        }
+    }
+
+    if(loading) return  <h3 className="my-5 text-center">로딩중입니다 ...</h3>
+    return (
+        <Row className='my-5 justify-content-center'>
+            <Col md={6} lg={4}>
+                <Card>
+                    <Card.Header>
+                        <h3 className='text-center'>로그인</h3>
+                    </Card.Header>
+                    <Card.Body>
+                        <form onSubmit={onSubmit}>
+                            <InputGroup className='mb-2'>
+                                <InputGroup.Text style={{ width: 100 }} className='justify-content-center' 
+                                >이메일</InputGroup.Text>
+                                <Form.Control name="email" placeholder='이메일을 입력해주세요.' 
+                                value={email} onChange={onChange}></Form.Control>
+                            </InputGroup>
+                            <InputGroup className='mb-2'>
+                                <InputGroup.Text style={{ width: 100 }} className='justify-content-center'>비밀번호</InputGroup.Text>
+                                <Form.Control name="pass" type="password"  placeholder='비밀번호를 입력해주세요.' 
+                                value={pass} onChange={onChange}></Form.Control>
+                            </InputGroup>
+                            <div>
+                                <Button className='my-2 w-100 btn-secondary' type='submit'>로그인</Button>
+                            </div>
+                        </form>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+    )
 }
 
 export default Login
---
+++ src/firebaseInit.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcNzeEYbRdqZGMpmG-SgqOjBDOHdBdN0A",
+  authDomain: "inha-c3ee8.firebaseapp.com",
+  projectId: "inha-c3ee8",
+  storageBucket: "inha-c3ee8.appspot.com",
+  messagingSenderId: "54515918510",
+  appId: "1:54515918510:web:c618fb047df32c21812f3f"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
